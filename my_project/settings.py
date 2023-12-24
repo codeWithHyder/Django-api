06c3564d4194c7570973609e60a02f0e2df2aaa8
Hyder <hyderali400@gmail.com>
2023-12-24 20:59:01 +0530

    add features to de[loy django api to vercel

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kh+o#sf0y4_l$qko2e89z*a!*%c*%lrqwscqbc50w-10mswnvc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
