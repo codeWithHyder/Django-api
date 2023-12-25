4064c3ed155f1f671d38254d0b18d0d4f89947f8
Hyder <hyderali400@gmail.com>
2023-12-25 14:08:09 +0530

    add features to deploy to vercel

--- my_project/settings.py
+++ my_project/settings.py
@@ -129,6 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+import os 
+
+# At the end of file. add these lines
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
